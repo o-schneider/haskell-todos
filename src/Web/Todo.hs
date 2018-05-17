e45fdc4c91946936935f7e113881ff2a4bf41d9e
Olivier Schneider <olivier.schneider@kolabnow.com>
2018-05-17 22:43:26 +0200

    Add delete route

--- src/Web/Todo.hs
+++ src/Web/Todo.hs
@@ -1,5 +1,4 @@
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE OverloadedStrings          #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module Web.Todo
   ( routes
@@ -11,22 +10,27 @@ import qualified Data.ByteString.Lazy as L
 import qualified Data.Text.Lazy       as T
 import           Data.UUID
 import           Data.UUID.V4
-import           Web.Scotty.Trans
+import           Web.Scotty.Trans     as S
 
 import qualified Todo.Todo            as Todo
-import           Todo.TodoRepository
+import           Todo.TodoRepository  as TodoRepository
 import           Web.State
 
 routes :: ScottyT T.Text MonadAppState ()
 routes = do
+  get "/todos" $
+    monadAppState (gets (getTodos . todoState)) >>= json
+
   post "/todos" $
     (\uuid (Todo.CreateTodoJSON t c) -> Todo.todo uuid t c) <$>
-      liftIO (toString <$> nextRandom) <*>
-      jsonData >>=
-      (\newTodo ->
-         (monadAppState . modify) ((AppState  . add newTodo) . todoState) >>=
-         (\x -> json newTodo))
+    liftIO (toString <$> nextRandom) <*>
+    jsonData >>=
+    (\newTodo ->
+       (monadAppState . modify) ((AppState . TodoRepository.add newTodo) . todoState) >>=
+       (\_ -> json newTodo))
 
-  get "/todos" $ do
-    c <- monadAppState $ gets (getTodos .todoState)
-    json c
+  S.delete "/todos/:id" $
+    param "id" >>=
+    (\id ->
+       (monadAppState . modify) ((AppState . TodoRepository.delete id) . todoState) >>=
+       (\_ -> (text . T.pack) ("Todo " ++ id ++ " deleted")))
